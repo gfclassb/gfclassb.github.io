58f21db4e913af12cb9e3134d8304388b4e9ce2c
gfclassb <120664366+gfclassb@users.noreply.github.com>
2025-09-07 11:46:10 +0800

    更新 nav.ts

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -1,13 +1,7 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
-  { text: '首页', link: '/' },
-  { text: 'VitePress', link: 'https://vitepress.dev/' },
-  {
-    text: '1.0.0-rc.**',
-    items: [
-      { text: '更新日志', link: 'https://github.com/vuejs/vitepress/blob/main/CHANGELOG.md' },
-      { text: '贡献', link: 'https://github.com/vuejs/vitepress/blob/main/.github/contributing.md' },
-      ],
-  },
+  { text: '主页', link: '/' },
+  { text: '目录', link: '/contents.md' },
+  { text: '时间线', link: '/timeline.md' }
 ]
