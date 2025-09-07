ba2e49886a7d6d072e9347a1ee14952319f5aa6e
gfclassb <120664366+gfclassb@users.noreply.github.com>
2025-09-07 11:50:47 +0800

    创建 sidebar.ts

---
+++ docs/.vitepress/configs/sidebar.ts
@@ -0,0 +1,13 @@
+import type { DefaultTheme } from 'vitepress'
+
+export const nav: DefaultTheme.Config['sidebar'] = [
+  { text: '首页', link: '/' },
+  { text: 'VitePress', link: 'https://vitepress.dev/' },
+  {
+    text: '1.0.0-rc.**',
+    items: [
+      { text: '更新日志', link: 'https://github.com/vuejs/vitepress/blob/main/CHANGELOG.md' },
+      { text: '贡献', link: 'https://github.com/vuejs/vitepress/blob/main/.github/contributing.md' },
+      ],
+  },
+]
